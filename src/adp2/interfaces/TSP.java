5944df4fd550033a51bd298272450a2c733666cc
Till Theis <till_theis2@yahoo.de>
2011-11-19 17:55:22 +0100

    document additional precondition

--- src/adp2/interfaces/TSP.java
+++ src/adp2/interfaces/TSP.java
@@ -12,7 +12,7 @@ public interface TSP {
      * between all of them.
      * 
      * @return the shortest path connecting the waypoints in m or
-     *         Path(EmptyList, -1) if m is not an nxn-Matrix
+     *         Path(EmptyList, -1) if m is not an nxn-Matrix or is not symmetric
      */
     Path minPath(Matrix m);
     
