b6b786d068fdff65bf6bccb8c06d9160333b3a88
Felix <Felix@Felix-PC>
2011-11-23 18:30:42 +0100

    Gui mit möglichkeit zum Path highlighten

---
+++ src/adp2/implementations/Darstellung.java
@@ -0,0 +1,67 @@
+package adp2.implementations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JFrame;
+
+import adp2.interfaces.Graph;
+import adp2.interfaces.Matrix;
+import adp2.interfaces.Path;
+
+import com.mxgraph.swing.mxGraphComponent;
+
+public class Darstellung extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	public final static int width = 1280;
+	public final static int height = 720;
+
+	public Graph graph;
+
+	public Darstellung(Graph g) {
+	super("ACO");
+	graph  = g;
+	mxGraphComponent graphComponent = new mxGraphComponent((GraphImpl) graph);
+	getContentPane().add(graphComponent);
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		//TestGraph von Andi und Basti
+		List<Integer> dist2 = Arrays.asList(0, 2, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 2, 0, 2, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 2, 0, 1, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 1, 0, 3, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 3, 0, 2, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 2, 0, 3, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 3, 0, 2, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 2, 0, 4, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 4, 0, 1, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 1, 0, 2, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 2, 0, 3, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 3, 0, 1, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 1, 0, 1, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 1, 0, 2, 20, 20, 20, 20, 20, 20, 20,
+				20, 20, 20, 20, 20, 20, 2, 0);
+		
+		List<Double> pher = new ArrayList<Double>();
+		for (int i = 0; i < 225; i++) {
+			pher.add(0.0);
+		}
+		Matrix<Integer> distM = Values.matrix(15, 15, dist2);
+		Matrix<Double> pherM = Values.matrix(15, 15, pher);
+
+		Darstellung frame = new Darstellung(GraphImpl.valueOf(distM, pherM));
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		Path p = Values.path(Arrays.asList(0, 1, 2, 3, 5, 9), 0); 
+		frame.graph.highlightPath(p);					// <-- minDist Path da rein
+	}
+
+}
